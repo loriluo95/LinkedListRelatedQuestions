fc363abe32f63b5c94bde29019ea97e234a8e848
lori <zluo2@wpi.edu>
2020-05-27 16:12:29 -0700

    Implement FindNthNode

--- src/FindNthNode.java
+++ src/FindNthNode.java
@@ -1,2 +1,22 @@
 public class FindNthNode {
+
+    public static int findNthNode(ListNode list, int n) {
+        boolean FOUND = false;
+
+        while (n > 1 && list != null) {
+            list = list.next;
+            n--;
+        }
+
+        if (n > 1)
+            throw new IllegalArgumentException("ListNode has less than nth node");
+
+        return list.val;
+    }
+    public static void main(String[] args) {
+        ListNode testNode = new ListNode(1);
+        testNode.next = new ListNode(2);
+        testNode.next.next = new ListNode(3);
+        System.out.println(findNthNode(testNode, 2));
+    }
 }
